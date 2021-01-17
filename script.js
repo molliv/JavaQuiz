7b705f40d8974665ff0e0d761f9231b96a6e1ab1
Molli V <mollitalkin@hotmail.com>
2021-01-16 21:57:32 -0800

    started timer function

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-var highScores = document.querySelector("#highScores")
-var startButton = document.querySelector("#start-button");
-var timer = document.querySelector("#timer");
+var highScores = document.querySelector(".highScores")
+var startButton = document.querySelector(".start-button");
+var timer = document.querySelector(".timer");
+
+
+function setTime() {
+    var counter = 25;
+    var timeInterval = setInterval(function() {
+        counter--;
+        if (counter >= 0) {
+            span = document.getElementById("timer");
+            span.innerHTML = counter;
+        }
+        if (counter === 0) {
+            alert("Sorry, you ran out of time!");
+            clearInterval(counter);
+        }
+    }, 1000);
+}
+
+function startButton () {
+    document.getElementsByClassName("start-button");
+    startTimer();
+};
